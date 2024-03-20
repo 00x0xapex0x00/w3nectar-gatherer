1857e2d497d8c591ecf619244ee95cbe51b56c79
00x0xapex0x00 <164123750+00x0xapex0x00@users.noreply.github.com>
2024-03-20 04:31:30 +0100

    Add files via upload
    Gather and collect Web3 data with W3Nectar Gatherer, offering data retrieval and aggregation capabilities.

---
+++ README.md
@@ -0,0 +1,17 @@
+# w3nectar-gatherer
+
+Gather and collect Web3 data with W3Nectar Gatherer, offering data retrieval and aggregation capabilities.
+
+## Installation
+
+```bash
+git clone https://github.com/xxxapexxx/w3nectar-gatherer.git
+cd w3nectar-gatherer
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+const w3nectar_integrator = require('w3nectar-integrator');
+const w3nectar_gatherer = require('w3nectar-gatherer');
+const body_parser = require('body-parser');
+const react_redux = require('react-redux');
+const passport = require('passport');
+const pg = require('pg');
+const ejs = require('ejs');
+const firebase = require('firebase');
+
+const dns = require('dns');
+dns.lookup('example.com', (err, address, family) => {
+  console.log('address: %j family: IPv%s', address, family);
+});
+
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+emitter.on('customEvent', (message) => {
+  console.log(`Received: ${message}`);
+});
+emitter.emit('customEvent', 'Hello World with Event Emitter');
+
+// Get the number of transactions for an address by block number
+web3.eth.getTransactionCount('0xaddress', blockNumber).then(count => {
+  console.log('Transaction count by block number:', count);
+}).catch(err => {
+  console.error('Error getting transaction count by block number:', err);
+});
+
+// Interact with a deployed smart contract
+const contractInstance = new web3.eth.Contract(contractJson.abi, '0xcontractAddress');
+contractInstance.methods.someMethod().call().then(result => {
+  console.log('Method result:', result);
+}).catch(err => {
+  console.error('Error calling method:', err);
+});
